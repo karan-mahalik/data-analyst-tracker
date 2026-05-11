2cc5908b29fd98fb615805daa22f1278e275be93
Karan Mahalik <karanmahalik88@gmail.com>
2026-05-11 23:03:09 +0530

    Configured env API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const API =
-  axios.create({
-
-    baseURL:
-      "https://data-analyst-tracker.onrender.com/",
-  });
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 export default API;
